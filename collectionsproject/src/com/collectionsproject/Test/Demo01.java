672673b1786a6b80366b6ebf4a1754a25530e23b
charan885 <charan885@gmail.com>
2021-10-08 10:56:49 +0530

    collectionsproject

---
+++ collectionsproject/src/com/collectionsproject/Test/Demo01.java
@@ -0,0 +1,23 @@
+package com.collectionsproject.Test;
+
+public class Demo01 {
+	/**
+	 * 
+	 * List
+	 * 
+	 * Set
+	 * 
+	 * 
+	 * Map
+	 * 
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
